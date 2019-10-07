2461cfd9b0428a68307907ba0b0023f78739bf8f
S00189902 <46888710+S00189902@users.noreply.github.com>
2019-10-07 11:56:18 +0100

    EX 1 - 6 Finsihed

---
+++ LabSheet$/Ex1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Team> teams = new List<Team>();
+
+            Team SligoRovers = new Team("Sligo Rovers");
+            Team FinnHarps = new Team("Finn Harps");
+            Team GalwayUnited = new Team("Galway United");
+            Team DerryCity = new Team("Derry City");
+            Team Dundalk = new Team("Dundalk");
+
+            teams.Add(SligoRovers);
+            teams.Add(FinnHarps);
+            teams.Add(GalwayUnited);
+            teams.Add(DerryCity);
+            teams.Add(Dundalk);
+
+            
+        }
+        static void Display(List<Team> teams)
+        {
+            Console.WriteLine("{0,-15}{1,-7}{2,-7}{3,-7}{4,-7}{5,-7}", "NAME", "POINTS", "WINS", "DRAWS", "LOSSES", "PLAYED");
+
+            foreach(Team team in teams)
+            {
+                Console.WriteLine(team.DisplayTable());
+            }
+        }
+    }
+}
---
+++ LabSheet$/Ex1/Team.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex1
+{
+   public class Team
+    {
+        private string teamName;
+
+        public string TeamName
+        {
+            get {return this.teamName; }
+           private set { this.teamName = value; }
+        }
+
+        private int numWins;
+
+        public int NumWins
+        {
+            get { return numWins; }
+            private set {numWins = value; }
+        }
+
+        private int numDraws;
+
+        public int NumDraws
+        {
+            get { return numDraws; }
+            private set { numDraws = value; }
+        }
+
+        private int numLosses;
+
+        public int NumLosses
+        {
+            get { return numLosses; }
+            private set { numLosses = value; }
+        }
+
+        private int numGames;
+
+        public int NumGames
+        {
+            get { return numGames; }
+            private set { numGames = value; }
+        }
+
+        private int points;
+
+        public int Points
+        {
+            get
+            {
+                points = (NumWins * 3) + (NumDraws * 1);
+                return points;
+            }
+
+        }
+
+        public Team(string name)
+        {
+            TeamName = name;
+        }
+
+        public string DisplayTable()
+        {
+            return string.Format($"{TeamName,-15}{Points,-7}{NumDraws,-7}{NumLosses,-7}{NumGames,-7}");
+        }
+
+
+       
+
+
+
+
+
+
+    }
+}
